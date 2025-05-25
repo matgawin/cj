0eb9633e8e0867cf5b2e46618d651617a1732073
Mateusz Gawin <matrix.gaw@gmail.com>
2025-05-25 23:32:29 +0200

    refactor: Simplified package.nix
    Removed 'inherit (pkgs) lib;' and added 'with pkgs;' to installPhase.

--- nix/package.nix
+++ nix/package.nix
@@ -2,75 +2,73 @@
   self,
   pkgs,
   ...
-}: let
-  inherit (pkgs) lib;
-in
-  pkgs.stdenv.mkDerivation rec {
-    pname = "journal-management";
-    version = "0.1.0";
-    src = self;
+}:
+pkgs.stdenv.mkDerivation rec {
+  pname = "journal-management";
+  version = "0.1.0";
+  src = self;
 
-    nativeBuildInputs = with pkgs; [
-      makeWrapper
-    ];
+  nativeBuildInputs = with pkgs; [
+    makeWrapper
+  ];
 
-    buildInputs = with pkgs; [
-      bash
-      coreutils
-      findutils
-      gnugrep
-      gnused
-      gawk
-      systemd
-      inotify-tools
-    ];
+  buildInputs = with pkgs; [
+    bash
+    coreutils
+    findutils
+    gnugrep
+    gnused
+    gawk
+    systemd
+    inotify-tools
+  ];
 
-    dontBuild = true;
+  dontBuild = true;
 
-    installPhase = ''
-      runHook preInstall
+  installPhase = with pkgs; ''
+    runHook preInstall
 
-      mkdir -p $out/bin $out/share/journal
+    mkdir -p $out/bin $out/share/journal
 
-      cp src/lib/common.sh $out/share/journal/common.sh
+    cp src/lib/common.sh $out/share/journal/common.sh
 
-      cp src/bin/create_journal_entry.sh $out/bin/cj
-      cp src/bin/journal_timestamp_monitor.sh $out/bin/journal-timestamp-monitor
+    cp src/bin/create_journal_entry.sh $out/bin/cj
+    cp src/bin/journal_timestamp_monitor.sh $out/bin/journal-timestamp-monitor
 
-      chmod +x $out/bin/cj
-      chmod +x $out/bin/journal-timestamp-monitor
+    chmod +x $out/bin/cj
+    chmod +x $out/bin/journal-timestamp-monitor
 
-      substituteInPlace $out/bin/cj \
-          --replace '#!/bin/bash' '#!${pkgs.bash}/bin/bash'
+    substituteInPlace $out/bin/cj \
+        --replace '#!/bin/bash' '#!${bash}/bin/bash'
 
-      substituteInPlace $out/bin/journal-timestamp-monitor \
-          --replace '#!/bin/bash' '#!${pkgs.bash}/bin/bash'
+    substituteInPlace $out/bin/journal-timestamp-monitor \
+        --replace '#!/bin/bash' '#!${bash}/bin/bash'
 
-      substituteInPlace $out/share/journal/common.sh \
-          --replace '#!/bin/bash' '#!${pkgs.bash}/bin/bash'
+    substituteInPlace $out/share/journal/common.sh \
+        --replace '#!/bin/bash' '#!${bash}/bin/bash'
 
-      substituteInPlace $out/bin/cj \
-          --replace 'COMMON_LIB="''${SCRIPT_DIR}/../lib/common.sh"' \
-                  'COMMON_LIB="'"$out/share/journal/common.sh"'"'
+    substituteInPlace $out/bin/cj \
+        --replace 'COMMON_LIB="''${SCRIPT_DIR}/../lib/common.sh"' \
+                'COMMON_LIB="'"$out/share/journal/common.sh"'"'
 
-      substituteInPlace $out/bin/journal-timestamp-monitor \
-          --replace 'COMMON_LIB="''${SCRIPT_DIR}/../lib/common.sh"' \
-                  'COMMON_LIB="'"$out/share/journal/common.sh"'"'
+    substituteInPlace $out/bin/journal-timestamp-monitor \
+        --replace 'COMMON_LIB="''${SCRIPT_DIR}/../lib/common.sh"' \
+                'COMMON_LIB="'"$out/share/journal/common.sh"'"'
 
-      wrapProgram $out/bin/cj \
-          --prefix PATH : ${lib.makeBinPath buildInputs}
+    wrapProgram $out/bin/cj \
+        --prefix PATH : ${lib.makeBinPath buildInputs}
 
-      wrapProgram $out/bin/journal-timestamp-monitor \
-          --prefix PATH : ${lib.makeBinPath buildInputs}
+    wrapProgram $out/bin/journal-timestamp-monitor \
+        --prefix PATH : ${lib.makeBinPath buildInputs}
 
-      runHook postInstall
-    '';
+    runHook postInstall
+  '';
 
-    meta = with pkgs.lib; {
-      description = "Journal management system with automatic timestamp updates and nixos support.";
-      homepage = "";
-      license = licenses.mit;
-      platforms = platforms.unix;
-      maintainers = [];
-    };
-  }
+  meta = with pkgs.lib; {
+    description = "Journal management system with automatic timestamp updates and nixos support.";
+    homepage = "";
+    license = licenses.mit;
+    platforms = platforms.unix;
+    maintainers = [];
+  };
+}
